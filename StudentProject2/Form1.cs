5d253684aa05d860433f9e04880df698f7c1a587
Tim Newman <timnewman9962@gmail.com>
2020-09-10 14:16:34 -0400

    Re-formatted code so that chart operations are contained in a class, rather than in the btnLoad_Click() method Added checks for missing file input and same Start, End dates Subbed-out feature to add pan and zoom functionality to the plot

--- StudentProject2/Form1.cs
+++ StudentProject2/Form1.cs
@@ -10,12 +10,13 @@ using System.Windows.Forms;
 using System.Net;
 using System.IO;
 using System.Security.Cryptography;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace StudentProject2
 {
     public partial class Form1 : Form
     {
-        public string stockDataFolder = ".\\StockData\\";
+        public static string stockDataFolder = ".\\StockData\\";    // This is folder where our saved stock data will go, in same folder as .exe
         public Form1()
         {
             InitializeComponent();
@@ -38,82 +39,130 @@ namespace StudentProject2
             txbDebug.Text = $"Symbol: {txbSymbol.Text}; Start Date: {dateStart.Text}; " +
                 $"End date: {dateEnd.Text}";
 
-            // Determine the base filename, which consists of the symbol and todays date
-            string fileBase = $"{txbSymbol.Text}_" + DateTime.Now.ToString("d").Replace('/', '-');
-
-            string sData ="empty";  // this will hold our stock data, whether from a file or AlphaVantage
-            // If the file exists, read the data from it; otherwise get the data online
-            if (File.Exists(stockDataFolder + fileBase + ".csv"))
+            // get the stock data as an array of strings
+            if(txbSymbol.Text == "STOCK / INDEX")
             {
-                var sr = new StreamReader(stockDataFolder + fileBase + ".csv");
-                sData = sr.ReadToEnd();
-                sr.Close();
+                MessageBox.Show("Please enter a Stock or Index symbol","Missing Input");
+                txbSymbol.Focus();
+                return;
             }
-            else
+            if(dateStart.Text == dateEnd.Text)
             {
-                var conn = new AVConnection("AYZBS9JXW6CTSR3P");  // get your own key at (https://www.alphavantage.co/support/#api-key)
-                sData = conn.SaveCSVfromURL(txbSymbol.Text);
-
-                // delete older versions
-                if (File.Exists(stockDataFolder + txbSymbol.Text + "_*.csv"))
-                {
-                    string[] old = Directory.GetFiles(stockDataFolder, txbSymbol.Text + "_*.csv");
-                    foreach (var item in old)
-                        if (item != null)
-                            File.Delete(item);
-                }
-                
-                // save a copy
-                File.WriteAllText(stockDataFolder + fileBase + ".csv", sData);
+                MessageBox.Show("Start and End dates must be different","Missing Input");
+                dateStart.Focus();
+                return;
             }
-            
-            // split the data into records of daily data
-            string[] sep = { "\r\n" };
-            string[] sRecords = sData.Split(sep, System.StringSplitOptions.RemoveEmptyEntries);
+            string[] sRecords = ProcessChart.GetStockData(txbSymbol.Text);
+
             // load into the list box
             lbData.Items.Clear();
             int nCnt = 0;
             foreach (var line in sRecords)
                 lbData.Items.Add($"{nCnt++}. {line}");
 
-            // setup variables for getting the lowest low and highest high; use to scale the vertical axis
-            var sEODdata = new string[6];
-            decimal hiMax = 0m;
-            decimal lowMin = 0m;
-            decimal low;
-            decimal high;
-            chart1.Series["s1"].Points.Clear();
+            // load into the chart
+            ProcessChart.PopulateChart(sRecords, dateStart.Text, dateEnd.Text, chart1);
+        }
 
-            // go through each record to plot it
-            var dtStart = DateTime.Parse(dateStart.Text);
-            var dtEnd = DateTime.Parse(dateEnd.Text);
-            for (int i = sRecords.Length - 1; i > 0; i--)
+        public static class ProcessChart
+        {
+            public static int panValue = 0;    // increase this value to scroll right, decrease to scroll left
+            public static int zoomValue = 0;   // increase this value to zoom out, decrease to zoom in
+            
+            public static string[] GetStockData(string symbol)
             {
-                // split into fields, ordered: Date, Open, High, Low, Close, Vol
-                sEODdata = sRecords[i].Split(',');
+                // Determine the base filename, which consists of the symbol and todays date
+                string fileBase = $"{symbol}_" + DateTime.Now.ToString("d").Replace('/', '-');
 
-                // use only the data requested
-                if (DateTime.Parse(sEODdata[0]) < dtStart)
-                    continue;
-                if (DateTime.Parse(sEODdata[0]) > dtEnd)
-                    break;
+                string sData = "empty";  // this will hold our stock data, whether from a file or AlphaVantage
+                                         // If the file exists, read the data from it; otherwise get the data online
+                if (File.Exists(stockDataFolder + fileBase + ".csv"))
+                {
+                    var sr = new StreamReader(stockDataFolder + fileBase + ".csv");
+                    sData = sr.ReadToEnd();
+                    sr.Close();
+                }
+                else
+                {
+                    var conn = new AVConnection("AYZBS9JXW6CTSR3P");  // get your own key at (https://www.alphavantage.co/support/#api-key)
+                    sData = conn.SaveCSVfromURL(symbol);
 
-                // process low and high
-                if (lowMin == 0)
-                    lowMin = decimal.Parse(sEODdata[3]);    // initialize low
-                low = decimal.Parse(sEODdata[3]);
-                lowMin = low < lowMin ? low : lowMin;
-                high = decimal.Parse(sEODdata[2]);
-                hiMax = high > hiMax ? high : hiMax;
+                    // delete older versions
+                    if (File.Exists(stockDataFolder + symbol + "_*.csv"))
+                    {
+                        string[] old = Directory.GetFiles(stockDataFolder, symbol + "_*.csv");
+                        foreach (var item in old)
+                            if (item != null)
+                                File.Delete(item);
+                    }
+                    // save a copy
+                    File.WriteAllText(stockDataFolder + fileBase + ".csv", sData);
+                }
 
-                // plot the candle, ordered: Date, Low, High, Open, Close
-                chart1.Series["s1"].Points.AddXY(sEODdata[0],
-                    low, high, decimal.Parse(sEODdata[1]),
-                    decimal.Parse(sEODdata[4]));
+                // split the data into records of daily data
+                string[] sep = { "\r\n" };
+                return sData.Split(sep, System.StringSplitOptions.RemoveEmptyEntries);
             }
-            // use the max and min values for Y-axis scaling
-            chart1.ChartAreas[0].AxisY.Minimum = Math.Ceiling((double)lowMin) - 1;
-            chart1.ChartAreas[0].AxisY.Maximum = Math.Ceiling((double)hiMax);
+
+            public static void PopulateChart(string[] records, string begin, string end, Chart chart1)
+            {
+                // convert our 'string' dates into 'DateTime' so they can be compared
+                var dtStart = DateTime.Parse(begin);
+                var dtEnd = DateTime.Parse(end);
+                // setup variables for getting the lowest low and highest high; use to scale the vertical axis
+                decimal hiMax = 0m;
+                decimal lowMin = 0m;
+                decimal low;
+                decimal high;
+                // setup our daily record, it is ordered: Date, Open, High, Low, Close, Vol
+                var sEODdata = new string[6];
+
+                // adjust chart based on user input
+                dtStart.AddDays(panValue - zoomValue);
+                dtEnd.AddDays(panValue + zoomValue);
+
+                chart1.Series["s1"].Points.Clear();
+
+                // go through each record to plot it
+                for (int i = records.Length - 1; i > 0; i--)
+                {
+                    // split into fields
+                    sEODdata = records[i].Split(',');
+
+                    // use only the data requested
+                    if (DateTime.Parse(sEODdata[0]) < dtStart)
+                        continue;
+                    if (DateTime.Parse(sEODdata[0]) > dtEnd)
+                        break;
+
+                    // process low and high
+                    if (lowMin == 0)
+                        lowMin = decimal.Parse(sEODdata[3]);    // initialize low
+                    low = decimal.Parse(sEODdata[3]);
+                    lowMin = low < lowMin ? low : lowMin;
+                    high = decimal.Parse(sEODdata[2]);
+                    hiMax = high > hiMax ? high : hiMax;
+
+                    // plot the candle, ordered: Date, Low, High, Open, Close
+                    chart1.Series["s1"].Points.AddXY(sEODdata[0],
+                        low, high, decimal.Parse(sEODdata[1]),
+                        decimal.Parse(sEODdata[4]));
+                }
+                // use the max and min values from above for Y-axis scaling
+                chart1.ChartAreas[0].AxisY.Minimum = Math.Ceiling((double)lowMin) - 1;
+                chart1.ChartAreas[0].AxisY.Maximum = Math.Ceiling((double)hiMax);
+            }
+
+            public static void SetPanAndZoom()
+            {
+                // This function needs to be called whenever one of the arrow keys is pressed.
+                // It is anticipated that the pan or scroll function will consider the number
+                // of bars on the chart, so that one key press will yield a visually incremental
+                // movement of the plot (possibly 5% or so).
+                // Likewise, a zoom increment should also consider the number of bars currently displayed.
+                // This function must also comprehend the date limits, so that an offset past the end
+                // of the chart is not possible.
+            }
 
         }
 
@@ -135,6 +184,7 @@ namespace StudentProject2
                 sr.Close();
                 return results;
             }
+
         }
 
     }
